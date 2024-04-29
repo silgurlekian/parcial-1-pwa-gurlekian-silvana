ce4a349fee3825f40e42d49ab4234102c671ceca
silgurlekian <sgurlekian@gmail.com>
2024-04-29 17:51:04 -0300

    feat: corregir estructura y estilos en detalle

--- js/script-detalle.js
+++ js/script-detalle.js
@@ -9,9 +9,11 @@ const mostrarDetalle = (pokemon) => {
             <div class="col-6">
                 <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
             </div>
-            <div class="col-6">
-                <p>Altura: ${pokemon.height}</p>
-                <p>Peso: ${pokemon.weight}</p>
+            <div class="col-6 detalles">
+                <h3>Altura:</h3> 
+                <p>${pokemon.height}</p>
+                <h3>Peso:</h3> 
+                <p>${pokemon.weight}</p>
                 <h3>Habilidades:</h3>
                 <ul>
                     ${pokemon.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
